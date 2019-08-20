e9bb101218d576562090ddbee812a09dc3dde1d1
gillianbc <gszemeti@gmail.com>
2019-08-20 21:38:58 +0100

    Lec 19 accepts both xml and json for GET user

--- src/main/java/com/gillianbc/app/ws/ui/controller/UserController.java
+++ src/main/java/com/gillianbc/app/ws/ui/controller/UserController.java
@@ -1,19 +1,38 @@
 package com.gillianbc.app.ws.ui.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gillianbc.app.ws.ui.model.response.UserRest;
 
 @RestController
 @RequestMapping("users")  //http://localhost:8080/users
 public class UserController {
 	
+	@GetMapping(path="{userId}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	public UserRest getUser(@PathVariable String userId) {
+		UserRest userRest =  new UserRest();
+		userRest.setFirstName("Hugh");
+		userRest.setLastName("Jass");
+		userRest.setEmail("hugh@gmail.com");
+		return userRest;
+	}
+	
+	// You can use: required = false   
+	// rather than a defaultValue, but don't use it with primitives
 	@GetMapping
-	public String getUser() {
-		return "Get user method was called";
+	public String getUsers(
+			@RequestParam(value="page", defaultValue="1") int page,
+			@RequestParam(value="limit", defaultValue="50") int limit) {
+		
+		return "Get users method was called with page: " + page + " limit: " + limit ;
 	}
 	
 	@PostMapping
